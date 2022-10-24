d15d0aad03bf1dd685ec9af07fe4c7b7c54a4e82
YuNandarMoe31 <yunandarmoe31@gmail.com>
2022-10-24 16:44:40 +0630

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
+import SiteNavigation from './components/SiteNavigation.vue'
 import { RouterView } from "vue-router";
 </script>
 
